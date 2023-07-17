fb3bf3dc9bce82c86547349c2bb1f76c88c13f1a
wdjty326 <wdjty326@gmail.com>
2023-07-17 14:26:46 +0900

    FIX(command): 커멘트 충돌 방지 코드 추가

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,30 +1,72 @@
 import "./hot";
 
-/** @deprecated 사용불가코드 */
+const createClipDownloadButton = () => {
+	const clipLayoutElement = document.createElement("div");
+	const clipButton = document.createElement("button");
+	clipButton.onclick = () => {
+		chrome.runtime.sendMessage({ command: "run-foo" }, (response) => {
+			console.log(response);
+		})
+	};
 
-const videoPlayer = document.querySelector(".video-player");
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.type === "childList") {
-      mutation.addedNodes.forEach((node) => {
-        if (node.nodeName === "VIDEO") {
-          (node as HTMLVideoElement).addEventListener("playing", () => {
-            const stream = (node as any).captureStream() as MediaStream;
-            const options = { mimeType: "video/webm; codecs=vp9" };
-            const mediaRecorder = new MediaRecorder(stream, options);
+	clipLayoutElement.append(clipButton);
+	return clipLayoutElement;
+}
 
-            mediaRecorder.addEventListener("dataavailable", (ev) => {
-              console.log(ev.data);
-            });
-            mediaRecorder.start();
-          });
-        }
-      });
-    }
-  });
+document.addEventListener("DOMContentLoaded", () => {
+	const channelPlayer = document.getElementById("channel-player");
+	if (!channelPlayer) return console.error("not found channel player");
+
+	const rightControlGroup = channelPlayer.querySelector(".player-controls__right-control-group");
+	if (!rightControlGroup) return console.error("not found right control group")
+
+	rightControlGroup.append(createClipDownloadButton());
 });
 
-observer.observe(videoPlayer!, {
-  childList: true,
-  subtree: true,
+
+// command 사용시 확장프로그램 충돌로 직접 키를 입력받도록 처리
+document.addEventListener("keydown", (event) => {
+	const inputKeys = [];
+	if (event.ctrlKey) inputKeys.push("Control");
+	if (event.metaKey) inputKeys.push("Command");
+	if (event.shiftKey) inputKeys.push("Shift");
+
+	inputKeys.push(event.key);
+
+	const commandLine = inputKeys.join("+");
+	if (commandLine === ["Control", "Shift", "X"].join("+") || // Mac 대응
+		commandLine === ["Command", "Shift", "X"].join("+")
+	) {
+		chrome.runtime.sendMessage({ command: "run-foo" }, (response) => {
+			console.log(response);
+		})
+	}
 });
+
+// const videoPlayer = document.querySelector("video");
+// console.log(videoPlayer);
+// const observer = new MutationObserver((mutations) => {
+//   mutations.forEach((mutation) => {
+//     if (mutation.type === "childList") {
+//       mutation.addedNodes.forEach((node) => {
+//         if (node.nodeName === "VIDEO") {
+//           (node as HTMLVideoElement).addEventListener("playing", () => {
+//             const stream = (node as any).captureStream() as MediaStream;
+//             const options = { mimeType: "video/webm; codecs=vp9" };
+//             const mediaRecorder = new MediaRecorder(stream, options);
+
+//             mediaRecorder.addEventListener("dataavailable", (ev) => {
+//               console.log(ev.data);
+//             });
+//             mediaRecorder.start();
+//           });
+//         }
+//       });
+//     }
+//   });
+// });
+
+// observer.observe(videoPlayer!, {
+//   childList: true,
+//   subtree: true,
+// });
--- src/main/index.ts
+++ src/main/index.ts
@@ -4,91 +4,100 @@ import "./hot"; // Hot Module Reloader
 import TwitchClipDatabase from "./database";
 
 const sleep = (ms: number) =>
-  new Promise<void>((resolve) => {
-    setTimeout(resolve, ms);
-  });
+	new Promise<void>((resolve) => {
+		setTimeout(resolve, ms);
+	});
 
 // 초기화 스케줄러
 let openWindowIds: number[] = [];
 const clearTwitchClipTemp = async () => {
-  if (process.env.NODE_ENV === "development") console.log("clear");
-  const windowIds: number[] = (await chrome.windows.getAll()).map(
-    (window) => window.id!
-  );
+	if (process.env.NODE_ENV === "development") console.log("clear");
+	const windowIds: number[] = (await chrome.windows.getAll()).map(
+		(window) => window.id!
+	);
 
-  openWindowIds
-    .filter((windowId) => windowIds.indexOf(windowId) === -1)
-    .forEach((windowId) => {
-      TwitchClipDatabase.delete("TwitchClipTemp", windowId);
-    });
+	openWindowIds
+		.filter((windowId) => windowIds.indexOf(windowId) === -1)
+		.forEach((windowId) => {
+			TwitchClipDatabase.delete("TwitchClipTemp", windowId);
+		});
 
-  openWindowIds = windowIds;
+	openWindowIds = windowIds;
 };
 
 let clearTwitchClipTempInterval: NodeJS.Timer;
 
 chrome.runtime.onInstalled.addListener(function () {
-  TwitchClipDatabase.clear(); // DB Clear
-  TwitchClipDatabase.clear("TwitchClipTemp"); // DB Clear
+	TwitchClipDatabase.clear(); // DB Clear
+	TwitchClipDatabase.clear("TwitchClipTemp"); // DB Clear
 
-  chrome.commands.onCommand.addListener(async (command) => {
-    if (command === "run-foo") {
-      clearInterval(clearTwitchClipTempInterval);
-      try {
-        const tabs = await await chrome.tabs.query({
-          active: true,
-          currentWindow: true,
-        });
+	const initClicpPopup = async () => {
+		clearInterval(clearTwitchClipTempInterval);
+		try {
+			const tabs = await await chrome.tabs.query({
+				active: true,
+				currentWindow: true,
+			});
 
-        if (
-          typeof tabs[0] !== "undefined" &&
-          typeof tabs[0].id !== "undefined"
-        ) {
-          const tabId = tabs[0].id;
-          const channelId = tabs[0].url!.substring(
-            tabs[0].url!.lastIndexOf("/") + 1,
-            tabs[0].url!.lastIndexOf("?") === -1
-              ? undefined
-              : tabs[0].url!.lastIndexOf("?")
-          );
+			if (
+				typeof tabs[0] !== "undefined" &&
+				typeof tabs[0].id !== "undefined"
+			) {
+				const tabId = tabs[0].id;
+				const channelId = tabs[0].url!.substring(
+					tabs[0].url!.lastIndexOf("/") + 1,
+					tabs[0].url!.lastIndexOf("?") === -1
+						? undefined
+						: tabs[0].url!.lastIndexOf("?")
+				);
 
-          const window = await chrome.windows.create({
-            url: `chrome-extension://${chrome.runtime.id}/clipper.html`,
-            width: 520,
-            height: 492,
-            type: "popup",
-          });
+				const window = await chrome.windows.create({
+					url: `chrome-extension://${chrome.runtime.id}/clipper.html`,
+					width: 520,
+					height: 492,
+					type: "popup",
+				});
 
-          await sleep(1000); // rendering
-          clearTwitchClipTemp();
-          await chrome.runtime.sendMessage({
-            windowId: window.id,
-            tabId,
-            channelId,
-            xProgramDateTime: new Date().toISOString(),
-          });
-        }
-      } catch (e) {
-        console.error(e);
-      }
+				await sleep(1000); // rendering
+				clearTwitchClipTemp();
+				await chrome.runtime.sendMessage({
+					windowId: window.id,
+					tabId,
+					channelId,
+					xProgramDateTime: new Date().toISOString(),
+				});
+			}
+		} catch (e) {
+			console.error(e);
+		}
 
-      clearTwitchClipTempInterval = setInterval(clearTwitchClipTemp, 10000);
-    }
-  });
+		clearTwitchClipTempInterval = setInterval(clearTwitchClipTemp, 10000);
+	};
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    (async () => {
-      const currentTab = await chrome.tabs.get(tabId);
+	chrome.commands.onCommand.addListener((command) => {
+		if (command === "run-foo") initClicpPopup();
+	});
 
-      const currentURL = currentTab.url || "";
-      const changeURL = changeInfo.url || "";
+	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+		if (message.command === "run-foo") {
+			initClicpPopup();
+			sendResponse("ok");
+		}
+	});
 
-      if (currentURL && /^https?:\/\/www\.twitch\.tv\/(.+)$/.test(currentURL)) {
-        if (currentURL !== changeURL)
-          TwitchClipDatabase.delete("TwitchClip", tabId); // reset
-      }
-    })();
-  });
+	chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+		(async () => {
+			const currentTab = await chrome.tabs.get(tabId);
 
-  webRequestListener();
+			const currentURL = currentTab.url || "";
+			const changeURL = changeInfo.url || "";
+
+			if (currentURL && /^https?:\/\/www\.twitch\.tv\/(.+)$/.test(currentURL)) {
+				if (currentURL !== changeURL)
+					TwitchClipDatabase.delete("TwitchClip", tabId); // reset
+			}
+		})();
+	});
+
+	webRequestListener();
 });
